3d960723221c272d6b2d7272fd05e8a3ca95bcfa
todoesverso <todoesverso@gmail.com>
2026-08-07 16:20:29 -0300

    Simple server refactor

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,12 @@
-use std::convert::Infallible;
-use std::net::SocketAddr;
+use anyhow::Result;
 
-use anyhow::{Context, Result};
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
+use crate::server::Server;
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
-}
+mod server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    let listener = TcpListener::bind(addr)
-        .await
-        .context(format!("Failed to bind to socket {addr}"))?;
-
-    loop {
-        let (stream, _) = listener
-            .accept()
-            .await
-            .context("Failed to accept connection")?;
-
-        let io = TokioIo::new(stream);
-
-        tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
-                eprintln!("Error serving connection: {:?}", err);
-            }
-        });
-    }
+    let server = Server::new();
+    server.run().await?;
+    Ok(())
 }
